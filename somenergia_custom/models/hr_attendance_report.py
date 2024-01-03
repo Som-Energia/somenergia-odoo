f9d3d064cb420f130d7657927fa1123f4513f338
pedropote <pere.montagud@somenergia.coop>
2024-01-03 06:18:42 +0100

    [IMP] Overtime manage imporvements

--- somenergia_custom/models/hr_attendance_report.py
+++ somenergia_custom/models/hr_attendance_report.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, tools
+from odoo import api, fields, models, tools, _
 
 
 class HRAttendanceReport(models.Model):
     _inherit = "hr.attendance.report"
+
+    def action_view_attendances(self):
+        # action_id = self.env.ref('hr_attendance.hr_attendance_action')
+        tree_view_id = self.env.ref('hr_attendance.view_attendance_tree')
+        date_to = fields.Date.add(self.check_in, days=1)
+        domain = [
+            ('employee_id', '=', self.employee_id.id),
+            ('check_in', '>=', self.check_in),
+            ('check_out', '<', date_to)
+        ]
+        action_open_attendances = {
+            'name': _('Day attendances %s' % self.check_in.strftime("%d/%m/%Y")),
+            'view_type': 'tree',
+            'res_model': 'hr.attendance',
+            'domain': domain,
+            'view_id': tree_view_id.id,
+            'views': [
+                (tree_view_id.id, 'tree'),
+            ],
+            'type': 'ir.actions.act_window',
+        }
+
+        return action_open_attendances
