aecbaedbf1fa273ab2abfdfb58a50e4fe7dc34f2
pedropote <pere.montagud@somenergia.coop>
2024-08-27 13:50:19 +0200

    [IMP] Overlapping list control for all users

--- somenergia_custom/__manifest__.py
+++ somenergia_custom/__manifest__.py
@@ -43,6 +43,7 @@
         'security/hr_attendance_report_security.xml',
         'security/hr_attendance_overtime_security.xml',
         'security/project_security.xml',
+        'security/hr_attendance_overlapping_report_security.xml',
         'views/hr_attendance_view.xml',
         'views/hr_leave_view.xml',
         'views/som_calendar_week_view.xml',
