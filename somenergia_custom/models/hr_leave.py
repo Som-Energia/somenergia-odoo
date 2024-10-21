608f0da1061e87a1de5744e8e8b109962647bda0
pedropote <pere.montagud@somenergia.coop>
2024-10-21 12:46:03 +0200

    [ADD] 'get_leaves' function include stress days with type 'no service' and departments

--- somenergia_custom/models/hr_leave.py
+++ somenergia_custom/models/hr_leave.py
@@ -3,8 +3,8 @@
 from odoo import api, fields, models
 from odoo.tools.translate import _
 from odoo.addons.resource.models.resource import float_to_time, HOURS_PER_DAY
-from odoo.addons.hr_holidays.models.hr_leave import DummyAttendance
 from datetime import datetime
+from odoo.addons.hr_holidays.models.hr_leave import DummyAttendance
 from pytz import timezone, UTC
 
 
@@ -102,6 +102,14 @@ class HolidaysRequest(models.Model):
                 }
 
     @api.model
+    def get_mail_employees_from_departments(self, department_ids):
+        # we got a list of mail lists by department
+        list_aux = [dep_id.member_ids.mapped("user_id.email") for dep_id in department_ids]
+        # we get a unique list with all mails
+        list_result = set(list([mail for sublist in list_aux for mail in sublist]))
+        return list_result
+
+    @api.model
     def get_leaves(self, start_date, end_date):
         """
         This function returns leaves from start_date to end_date in the following
@@ -121,6 +129,30 @@ class HolidaysRequest(models.Model):
                 'start_time': leave_id.date_from,
                 'end_time': leave_id.date_to
             })
+
+        # get from stress_days with departments
+        sd_leave_ids = self.env['hr.leave.stress.day'].search([
+            ('start_date', '>=', start_date),
+            ('end_date', '<=', end_date),
+            ('som_type', '=', 'no_service'),
+            ('department_ids', '!=', False),
+        ])
+
+        # overlapping stress_day dates with leaves does not matter
+        for sd_leave_id in sd_leave_ids:
+            list_mails_employees = self.get_mail_employees_from_departments(sd_leave_id.department_ids)
+            start_datetime = datetime(
+                sd_leave_id.start_date.year, sd_leave_id.start_date.month, sd_leave_id.start_date.day,7, 0, 0
+            )
+            end_datetime = datetime(
+                sd_leave_id.end_date.year, sd_leave_id.end_date.month, sd_leave_id.end_date.day,14, 0, 0
+            )
+            for mail in list_mails_employees:
+                res.append({
+                    'worker': mail,
+                    'start_time': start_datetime,
+                    'end_time': end_datetime,
+                })
 
         return res
 
