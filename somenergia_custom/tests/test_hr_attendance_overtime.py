5ddefa3210379a61b244ecb94dc886f1527c5cde
pedropote <pere.montagud@somenergia.coop>
2025-01-08 12:58:18 +0100

    ✅ Tests overtime restrictions with attendance

--- somenergia_custom/tests/test_hr_attendance_overtime.py
+++ somenergia_custom/tests/test_hr_attendance_overtime.py
@@ -21,7 +21,7 @@ class TestHrAttendanceOvertime(common.TransactionCase):
 
         # 7 hours_per_day by default
         cls.calendar1 = resource_calendar.create(
-            {"name": "Test calendar 1", "attendance_ids": []}
+            {"name": "Test calendar 1"}
         )
 
         cls.employee_user = new_test_user(
@@ -35,10 +35,10 @@ class TestHrAttendanceOvertime(common.TransactionCase):
             'tz': 'Europe/Brussels',
         })
 
+        cls.employee_emp.write({"calendar_ids": [(2, cls.employee_emp.calendar_ids.id)]})
         cls.employee_emp.calendar_ids = [
             (0, 0, {"date_start": "2025-01-01", "calendar_id": cls.calendar1.id}),
         ]
-
         cls.employee_emp._compute_current_calendar()
 
     def _tz_datetime(self, year, month, day, hour, minute):
@@ -68,7 +68,6 @@ class TestHrAttendanceOvertime(common.TransactionCase):
 
     def test_restrictive_overtime_setup_enabled__max_hours_today(self):
         """ assuming max_hours_today = 8 (7 + 1) """
-        """ assuming max_hours_today = 8 (7 + 1) """
         self.env.company.som_restrictive_overtime = True
         year_aux, month_aux, day_aux = (2025, 1, 2)
 
@@ -81,7 +80,7 @@ class TestHrAttendanceOvertime(common.TransactionCase):
 
         self.assertEqual(att1_id.get_max_hours_today(), 8)
 
-    def test_restrictive_overtime_setup_enabled(self):
+    def test_restrictive_overtime_setup_enabled__1_extra_hour_allowed(self):
         """
         max_hours_today = 8 (7 + 1)
         check_in and check_out in UTC
@@ -126,7 +125,9 @@ class TestHrAttendanceOvertime(common.TransactionCase):
         """
         self.env.company.som_restrictive_overtime = True
         self.calendar1.write({'som_max_overtime_per_day': 2}) # max_hours_today = 9 (7 + 2)
+
         self.assertEqual(self.calendar1.som_max_overtime_per_day, 2)
+        self.assertEqual(self.employee_emp.sudo().som_current_calendar_id.id, self.calendar1.id)
 
         year_aux, month_aux, day_aux = (2025, 1, 2)
 
@@ -154,10 +155,10 @@ class TestHrAttendanceOvertime(common.TransactionCase):
                 'check_out': datetime(year_aux, month_aux, day_aux, 18, 00),
             })
 
-        error_msg = _("L'hora màxima per tancar l'assistència és 02/01/2025 18:15:00 ")
+        error_msg = _("L'hora màxima per tancar l'assistència és 02/01/2025 18:30:00 ")
         self.assertEqual(str(context.exception), error_msg)
 
         with patch.object(fields.Datetime, 'now',
                           lambda: self._tz_datetime(year_aux, month_aux, day_aux, 19, 0).astimezone(
                               pytz.utc).replace(tzinfo=None)):
-            self.assertEqual(self.employee_emp.hours_today, 8.75, "It should have counted 8.75 hours")
+            self.assertEqual(self.employee_emp.hours_today, 8.5, "It should have counted 8.5 hours")
