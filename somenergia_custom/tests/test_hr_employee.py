7cd061c5599f141bd62e44a1a0ad715c20735ca8
pedropote <pere.montagud@somenergia.coop>
2025-04-28 16:30:49 +0200

    ✅ New test in order to check employee 'is_present'search

--- somenergia_custom/tests/test_hr_employee.py
+++ somenergia_custom/tests/test_hr_employee.py
@@ -17,22 +17,16 @@ class TestEmployeeIsPresent(TestHrHolidaysCommon):
     def setUpClass(cls):
         super().setUpClass()
 
-        leave_start_datetime = datetime(2025, 4, 1, 7, 0, 0, 0)
-        leave_end_datetime = leave_start_datetime + relativedelta(days=3)
+        leave_start_datetime = datetime(2025, 4, 1, 8, 0, 0, 0)
+        leave_end_datetime = datetime(2025, 4, 3, 15, 0, 0, 0)
+        # leave_end_datetime = leave_start_datetime + relativedelta(days=2)
 
-        leave_start_datetime_2 = datetime(2024, 4, 1, 7, 0, 0, 0)
-        leave_end_datetime_2 = leave_start_datetime_2 + relativedelta(days=1)
+        leave_start_datetime_2 = datetime(2025, 4, 1, 7, 0, 0, 0)
+        leave_end_datetime_2 = datetime(2025, 4, 1, 15, 0, 0, 0)
+        # leave_end_datetime_2 = leave_start_datetime_2 + relativedelta(days=0)
 
         cls.hr_leave_type_1 = cls.env['hr.leave.type'].with_user(cls.user_hrmanager).create({
             'name': 'Leave Type 1',
-        })
-
-        cls.hr_leave_type_2 = cls.env['hr.leave.type'].with_user(cls.user_hrmanager).create({
-            'name': 'Leave Type 2',
-        })
-
-        cls.hr_leave_type_3 = cls.env['hr.leave.type'].with_user(cls.user_hrmanager).create({
-            'name': 'Leave Type 3',
         })
 
         cls.holiday_1 = cls.env['hr.leave'].with_context(
@@ -43,24 +37,9 @@ class TestEmployeeIsPresent(TestHrHolidaysCommon):
             'holiday_status_id': cls.hr_leave_type_1.id,
             'date_from': leave_start_datetime,
             'date_to': leave_end_datetime,
-            'request_date_from': leave_start_datetime.date(),
-            'request_date_to': leave_end_datetime.date(),
-            'number_of_days': (leave_end_datetime - leave_start_datetime).days,
+            'number_of_days': 3,
         })
         cls.holiday_1.with_user(cls.user_hrmanager).action_validate()
-
-        cls.holiday_2 = cls.env['hr.leave'].with_context(
-            mail_create_nolog=True, mail_notrack=True
-        ).with_user(cls.user_employee).create({
-            'name': 'Leave 2',
-            'employee_id': cls.employee_emp.id,
-            'holiday_status_id': cls.hr_leave_type_2.id,
-            'date_from': leave_start_datetime_2,
-            'date_to': leave_end_datetime_2,
-            'request_date_from': leave_start_datetime_2.date(),
-            'request_date_to': leave_end_datetime_2.date(),
-            'number_of_days': (leave_end_datetime_2 - leave_start_datetime_2).days,
-        })
 
         cls.user_employee2 = new_test_user(cls.env, login='pere', groups='base.group_user')
         cls.employee_emp2 = cls.env['hr.employee'].create({
@@ -69,17 +48,15 @@ class TestEmployeeIsPresent(TestHrHolidaysCommon):
             'department_id': cls.rd_dept.id,
         })
 
-        cls.holiday_3 = cls.env['hr.leave'].with_context(
+        cls.holiday_2 = cls.env['hr.leave'].with_context(
             mail_create_nolog=True, mail_notrack=True
         ).with_user(cls.user_employee2).create({
-            'name': 'Leave 3',
+            'name': 'Leave 2',
             'employee_id': cls.employee_emp2.id,
-            'holiday_status_id': cls.hr_leave_type_2.id,
+            'holiday_status_id': cls.hr_leave_type_1.id,
             'date_from': leave_start_datetime_2,
             'date_to': leave_end_datetime_2,
-            'request_date_from': leave_start_datetime_2.date(),
-            'request_date_to': leave_end_datetime_2.date(),
-            'number_of_days': (leave_end_datetime_2 - leave_start_datetime_2).days,
+            'number_of_days': 1,
         })
 
         cls.env['hr.leave.type'].search([]).write({'som_eoa_notification_mail': False})
@@ -90,7 +67,19 @@ class TestEmployeeIsPresent(TestHrHolidaysCommon):
         return tz.localize(datetime(year, month, day, hour, minute)).astimezone(pytz.utc).replace(tzinfo=None)
 
     @freeze_time('2025-04-01 06:00:00')
-    def test_is_present_search_no_is_present(self):
-        emp_ids = self.env['hr.employee'].search([("is_present", "=", True)])
-        self.assertNotIn(self.employee_emp.id, emp_ids.ids, "Value should not be in the list")
-        self.assertIn(self.employee_emp2.id, emp_ids.ids, "Value should be in the list")
+    def test_is_present_search_both_no_present(self):
+        present_emp_ids = self.env['hr.employee'].search([("is_present", "=", True)])
+        self.assertNotIn(self.employee_emp.id, present_emp_ids.ids, "Value should not be in the list")
+        self.assertNotIn(self.employee_emp2.id, present_emp_ids.ids, "Value should not be in the list")
+
+    @freeze_time('2025-04-02 06:00:00')
+    def test_is_present_search_just_one_present(self):
+        present_emp_ids = self.env['hr.employee'].search([("is_present", "=", True)])
+        self.assertNotIn(self.employee_emp.id, present_emp_ids.ids, "Value should not be in the list")
+        self.assertIn(self.employee_emp2.id, present_emp_ids.ids, "Value should be in the list")
+
+    @freeze_time('2025-04-04 06:00:00')
+    def test_is_present_search_both_present(self):
+        present_emp_ids = self.env['hr.employee'].search([("is_present", "=", True)])
+        self.assertIn(self.employee_emp.id, present_emp_ids.ids, "Value should be in the list")
+        self.assertIn(self.employee_emp2.id, present_emp_ids.ids, "Value should be in the list")
