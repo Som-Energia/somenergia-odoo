bc571eff350140719273ac97aeccdbca52754697
pedropote <pere.montagud@somenergia.coop>
2025-09-02 09:10:16 +0200

    ✨ Auto assignment in leads creted from calls
    Co-authored-by:  Pau Boix <pau.boix@somenergia.coop>

--- som_crm/models/crm_phonecall.py
+++ som_crm/models/crm_phonecall.py
@@ -7,15 +7,26 @@ class CrmPhonecall(models.Model):
     _name = 'crm.phonecall'
     _inherit = ['crm.phonecall']
 
+
+    def _get_user_crm(self):
+        user_id = self.env['res.users'].search([
+            ('som_call_center_user', '=', self.som_operator),
+        ], limit = 1)
+        return user_id or False
+
     def _prepare_opportunity_vals(self):
         res = super()._prepare_opportunity_vals()
 
         utm_medium_phone_id = self.env.ref('utm.utm_medium_phone', raise_if_not_found=False) or False
+
+        user_id = self._get_user_crm()
+
         res.update({
             'medium_id': utm_medium_phone_id.id if utm_medium_phone_id else False,
             'contact_name': self.som_caller_name,
             'email_from': self.email_from,
             'phone': self.som_phone,
             'vat': self.som_caller_vat,
+            'user_id': user_id.id if user_id else False,
         })
         return res
--- som_crm/models/res_users.py
+++ som_crm/models/res_users.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
 class ResUsers(models.Model):
@@ -9,3 +10,16 @@ class ResUsers(models.Model):
         string='Call center user',
         required=False,
     )
+
+    @api.constrains('som_call_center_user')
+    def _check_call_center_user(self):
+        for record in self:
+            count = self.search_count(
+                [
+                    ("id", "!=", record.id),
+                    ("som_call_center_user", "!=", ""),
+                    ("som_call_center_user", "=", record.som_call_center_user),
+                ]
+            )
+            if count:
+                raise ValidationError(_("The field 'Call center user' must be unique."))
