c8c6290246151aa3360b865ebd4eac5474425843
pedropote <pere.montagud@somenergia.coop>
2025-09-03 16:31:03 +0200

    🌐 Global translations
    Co-authored-by:  Pau Boix <pau.boix@somenergia.coop>

--- som_crm/models/contact_time_slot.py
+++ som_crm/models/contact_time_slot.py
@@ -7,7 +7,7 @@ class ContactTimeSlot(models.Model):
     _description = 'Contact Time Slot'
     _order = 'sequence, name'
 
-    name = fields.Char('Nom', required=True)
-    sequence = fields.Integer('Seqüència', default=10)
-    active = fields.Boolean('Actiu', default=True)
-    color = fields.Integer("Color")
+    name = fields.Char('Name', required=True)
+    sequence = fields.Integer('Sequence', default=10)
+    active = fields.Boolean('Active', default=True)
+    color = fields.Integer('Color')
