4befd6739e5de4423e0cca4ae9afd36a20c7c0c1
PauBoix <pau.boix@somenergia.coop>
2025-09-12 13:30:55 +0200

    🚧 Proof of concept
    Co-authored-by: Pere Montagud <pere.montagud@somenergia.coop>

--- som_crm/models/crm_lead.py
+++ som_crm/models/crm_lead.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api, _
+from erppeek import Client
+from odoo.tools import config
 
 
 class Lead(models.Model):
@@ -39,3 +41,19 @@ class Lead(models.Model):
 
     def assign_to_me(self):
         self.write({"user_id": self.env.user.id})
+
+    @api.model
+    def _erp_connect(self):
+        erppeek = dict(
+            server=f"{config.get('erp_uri')}:{config.get('erp_port')}",
+            db=config.get('erp_dbname'),
+            user=config.get('erp_user'),
+            password=config.get('erp_pwd'),
+        )
+        c = Client(**erppeek)
+
+        obj_res_partner = c.model("res.partner")
+        partner_id = obj_res_partner.browse(37577)
+        print(partner_id.name)
+
+        pass
