640034ce35fbc499eaaaaa0de0ef1e707772537f
PauBoix <pau.boix@somenergia.coop>
2025-09-12 16:34:55 +0200

    🚧 more proofs of concept

--- som_crm/models/crm_lead.py
+++ som_crm/models/crm_lead.py
@@ -42,8 +42,29 @@ class Lead(models.Model):
     def assign_to_me(self):
         self.write({"user_id": self.env.user.id})
 
+    def get_leads_to_update(self):
+
+        searchable_stages = [
+            "Pending 1st contact",
+            "Pending 1st contact and simulation",
+            "Pending simulation or comparison",
+            "Simulation or comparison sent",
+            "Interested and will contract",
+        ]
+        crm_stage_ids = self.env['crm.stage'].search([('name', 'in', searchable_stages)])
+
+        return self.search(
+            [
+                ("stage_id", "in", crm_stage_ids.ids),
+
+            ]
+        )
+
     @api.model
     def _erp_connect(self):
+
+        lead_ids = self.get_leads_to_update()
+        print(f"leads to update: {lead_ids}")
         erppeek = dict(
             server=f"{config.get('erp_uri')}:{config.get('erp_port')}",
             db=config.get('erp_dbname'),
@@ -51,9 +72,34 @@ class Lead(models.Model):
             password=config.get('erp_pwd'),
         )
         c = Client(**erppeek)
+        polissa_obj = c.model("giscedata.polissa")
 
-        obj_res_partner = c.model("res.partner")
-        partner_id = obj_res_partner.browse(37577)
-        print(partner_id.name)
+        found_ids = []
+        for lead in lead_ids:
+            if not lead.som_cups:
+                continue
+            polissa_id = False
+            polissa_id = polissa_obj.search(
+                    [('cups.name', '=ilike', '{}%'.format(lead.som_cups[:20]))]
+                )
+            if polissa_id:
+                found_ids.append(lead.id)
+                lead.write({'stage_id': self.env.ref('som_crm.som_stage_lead6').id})
 
-        pass
+        print(f"Leads moved to 'Contracted' stage: {found_ids}")
+        lead_ids = lead_ids.filtered(lambda r: r.id not in found_ids)
+
+        found_ids = []
+        for lead in lead_ids:
+            if not lead.vat:
+                continue
+            polissa_id = False
+            polissa_id = polissa_obj.search(
+                [('titular_nif', '=', '{}'.format(lead.vat))]
+                )
+            if polissa_id:
+                found_ids.append(lead.id)
+                lead.write({'stage_id': self.env.ref('som_crm.som_stage_lead6').id})
+
+        print(f"Leads moved to 'Contracted' stage: {found_ids}")
+        lead_ids = lead_ids.filtered(lambda r: r.id not in found_ids)
