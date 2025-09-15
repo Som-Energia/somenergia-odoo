43d8f19dfdbc88dd46eaf3df5e5a5e802e63b35b
PauBoix <pau.boix@somenergia.coop>
2025-09-15 15:23:53 +0200

    ✨ Update leads based on erp leads
    Co-authored-by: Pere Montagud <pere.montagud@somenergia.coop>

--- som_crm/models/crm_lead.py
+++ som_crm/models/crm_lead.py
@@ -67,26 +67,41 @@ class Lead(models.Model):
         ])
         return lead_ids
 
-    def _erp_search_by_cups(self, polissa_obj, cups_value):
-        erp_field = 'cups.name'
+    def _erp_search_by_cups(self, erp_lead_obj, cups_value):
+        erp_field = 'cups'
         cups_truncated = cups_value[:20]
         domain = [(erp_field, '=ilike', f'{cups_truncated}%')]
-        return polissa_obj.search(domain, limit=1)
+        return erp_lead_obj.search(domain, limit=1)
 
-    def _erp_search_by_vat(self, polissa_obj, vat_value):
-        erp_field = 'titular_nif'
+    def _erp_search_by_vat(self, erp_lead_obj, vat_value):
+        erp_field = 'titular_vat'
         normalized_vat = vat_value if vat_value.startswith('ES') else f'ES{vat_value}'
         domain = [(erp_field, '=', normalized_vat)]
-        return polissa_obj.search(domain, limit=1)
+        return erp_lead_obj.search(domain, limit=1)
 
-    def get_contract_in_erp(self, polissa_obj):
+    def _erp_search_by_email(self, erp_lead_obj, email_value):
+        erp_field = 'titular_email'
+        domain = [(erp_field, '=', email_value)]
+        return erp_lead_obj.search(domain, limit=1)
+
+    def _erp_search_by_phone(self, erp_lead_obj, phone_value):
+        erp_field = 'titular_phone'
+        casted_phone = phone_value
+        if len(phone_value) > 9:
+            casted_phone = phone_value.replace(' ','')[3:]
+        domain = [(erp_field, '=', casted_phone)]
+        return erp_lead_obj.search(domain, limit=1)
+
+    def get_contract_in_erp(self, erp_lead_obj):
         self.ensure_one()
         search_strategies = {
             'som_cups': self._erp_search_by_cups,
             'vat': self._erp_search_by_vat,
+            'email_from': self._erp_search_by_email,
+            'phone': self._erp_search_by_phone,
         }
 
-        for lead_field in ['som_cups', 'vat']:
+        for lead_field in ['som_cups', 'vat', 'email_from', 'phone']:
             if lead_field not in search_strategies:
                 _logger.warning(f"No search strategy for field {lead_field}")
                 continue
@@ -95,40 +110,11 @@ class Lead(models.Model):
             if not value_to_search:
                 continue
 
-            polissa_id = search_strategies[lead_field](polissa_obj, value_to_search)
-            if polissa_id:
-                return polissa_id
+            erp_lead_id = search_strategies[lead_field](erp_lead_obj, value_to_search)
+            if erp_lead_id:
+                return erp_lead_id
 
         return False
-
-    # def is_contracted_in_erp_mine(self, polissa_obj):
-    #     self.ensure_one()
-    #     polissa_id = False
-
-    #     for lead_field, erp_field in MAPPED_FIELDS_LEAD_ERP.items():
-    #         polissa_id = False
-    #         value_to_search = getattr(self, lead_field)
-    #         if not value_to_search:
-    #             continue
-    #         domain = []
-
-    #         if lead_field == 'som_cups':
-    #             value_to_search = value_to_search[:20]
-    #             domain = [(erp_field, '=ilike', '{}%'.format(value_to_search))]
-    #             polissa_id = polissa_obj.search(domain)
-    #             if polissa_id:
-    #                 return polissa_id
-
-    #         if lead_field == 'vat':
-    #             value_to_search = 'ES{}'.format(value_to_search) if (
-    #                 not value_to_search.startswith('ES')
-    #             ) else value_to_search
-    #             domain = [(erp_field, '=', value_to_search)]
-    #             polissa_id = polissa_obj.search(domain)
-    #             if polissa_id:
-    #                 return polissa_id
-
-    #     return polissa_id or False
 
     @api.model
     def _erp_sync(self):
@@ -156,14 +142,15 @@ class Lead(models.Model):
             _logger.error(f"Error connecting to ERP: {e}")
             return
 
-        polissa_obj = c.model("giscedata.polissa")
+        erp_lead_obj = c.model("giscedata.crm.lead")
 
         found_ids = []
-        for lead in lead_ids:
-            pol_id = lead.get_contract_in_erp(polissa_obj)
-            if pol_id:
-                found_ids.append(lead.id)
+        for lead_id in lead_ids:
+            erp_lead_id = lead_id.get_contract_in_erp(erp_lead_obj)
+            if erp_lead_id:
+                found_ids.append(lead_id.id)
                 # TODO: new field to store ERP contract ID?
+
 
         if not found_ids:
             _logger.info("No leads with contract in ERP found")
@@ -173,50 +160,3 @@ class Lead(models.Model):
         lead_to_update_ids.write({'stage_id': won_stage_id.id})
 
         _logger.info(f"Leads moved to 'Won' stage: {lead_to_update_ids.ids}")
-
-    @api.model
-    def _erp_proof_of_concept_connect(self):
-        lead_ids = self.get_leads_to_update()
-        if not lead_ids:
-            return
-        _logger.info(f"Leads to update: {lead_ids}")
-
-        _logger.info(f"Connecting to ERP")
-        erppeek = dict(
-            server=f"{config.get('erp_uri')}:{config.get('erp_port')}",
-            db=config.get('erp_dbname'),
-            user=config.get('erp_user'),
-            password=config.get('erp_pwd'),
-        )
-        c = Client(**erppeek)
-        polissa_obj = c.model("giscedata.polissa")
-
-        found_ids = []
-        for lead in lead_ids:
-            if not lead.som_cups:
-                continue
-            polissa_id = False
-            polissa_id = polissa_obj.search(
-                    [('cups.name', '=ilike', '{}%'.format(lead.som_cups[:20]))]
-                )
-            if polissa_id:
-                found_ids.append(lead.id)
-                lead.write({'stage_id': self.env.ref('som_crm.som_stage_lead6').id})
-
-        _logger.info(f"Leads moved to 'Contracted' stage: {found_ids}")
-        lead_ids = lead_ids.filtered(lambda r: r.id not in found_ids)
-
-        found_ids = []
-        for lead in lead_ids:
-            if not lead.vat:
-                continue
-            polissa_id = False
-            polissa_id = polissa_obj.search(
-                [('titular_nif', '=', '{}'.format(lead.vat))]
-                )
-            if polissa_id:
-                found_ids.append(lead.id)
-                lead.write({'stage_id': self.env.ref('som_crm.som_stage_lead6').id})
-
-        _logger.info(f"Leads moved to 'Contracted' stage: {found_ids}")
-        lead_ids = lead_ids.filtered(lambda r: r.id not in found_ids)
