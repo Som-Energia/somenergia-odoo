fd456ca4308d7444db35fb847a3c583daa98aa79
PauBoix <pau.boix@somenergia.coop>
2025-09-26 12:10:35 +0200

    🩹 Fix erp sync
    Co-authored-by: Pere Montagud <pere.montagud@somenergia.coop>

--- som_crm/models/crm_lead.py
+++ som_crm/models/crm_lead.py
@@ -122,8 +122,6 @@ class Lead(models.Model):
         lead_ids = self.env['crm.lead'].with_context(active_test=False).search([
             ("stage_id", "!=", won_stage_id.id),
             ("type", "=", 'opportunity'),
-            # ("id", "=", 105),
-            # ("active", "!=", False), ??????
         ])
         return lead_ids
 
@@ -169,8 +167,7 @@ class Lead(models.Model):
             value_to_search = getattr(self, lead_field, None)
             if not value_to_search:
                 continue
-            # domain = [('crm_lead_id', '!=', False)]
-            domain = []
+            domain = [('crm_lead_id', '=', 0)]
             erp_lead_id = search_strategies[lead_field](erp_lead_obj, domain, value_to_search)
             if erp_lead_id:
                 return erp_lead_id[0]
