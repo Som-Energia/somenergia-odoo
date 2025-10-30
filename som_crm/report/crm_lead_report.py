108a8c249f7c6b2d6e0e9e6420456d44ffa38bb4
pedropote <pere.montagud@somenergia.coop>
2025-10-30 13:56:16 +0100

    🧐 Funnel and result pivots
    Co-authored-by:  Pau Boix <pau.boix@somenergia.coop>

--- som_crm/report/crm_lead_report.py
+++ som_crm/report/crm_lead_report.py
@@ -14,11 +14,30 @@ class CrmLeadReport(models.Model):
     user_id = fields.Many2one(comodel_name="res.users", string="User", readonly=True)
     team_id = fields.Many2one(comodel_name="crm.team", string="Team", readonly=True)
     nbr_cases = fields.Integer(string="#", readonly=True)
-    state = fields.Many2one(comodel_name="crm.stage", string="State", readonly=True)
+    stage = fields.Many2one(comodel_name="crm.stage", string="State", readonly=True)
     create_date = fields.Datetime(readonly=True, index=True)
     date_closed = fields.Datetime(string="Close Date", readonly=True, index=True)
     day_close = fields.Float(string="Days to be closed", readonly=True, index=True)
-    won_daily_target = fields.Integer(string="Daily target", readonly=True, index=True)
+    channel = fields.Many2one(comodel_name="utm.medium", string="Channel", readonly=True)
+    source = fields.Many2one(comodel_name="utm.source", string="Source", readonly=True)
+    lost_reason = fields.Many2one(comodel_name="crm.lost.reason", string="Lost Reason", readonly=True)
+    won_daily_target = fields.Float(string="Daily target %", readonly=True, index=True)
+    won_daily_target_static = fields.Integer(
+        string="Daily target", readonly=True, index=True,
+        group_operator="max",
+    )
+    virtual_state = fields.Selection(
+        selection=[("w", "Won"), ("l", "Lost"), ("f", "Flying")]
+    )
+    count_won = fields.Integer(string="Count Won", readonly=True, index=True)
+    count_flying = fields.Integer(string="Count Flying", readonly=True, index=True)
+    count_lost = fields.Integer(string="Count Lost", readonly=True, index=True)
+    count_total = fields.Integer(
+        string="Count Total",
+        group_operator="max",
+        readonly=True, index=True
+    )
+    active_stage = fields.Char(string="Active Stage", readonly=True, index=True)
 
     def _select(self):
         select_str = """
@@ -31,10 +50,37 @@ class CrmLeadReport(models.Model):
             ,cl.create_date
             ,cl.date_closed
             ,cl.day_close
-            ,COALESCE(
-                (SELECT value FROM ir_config_parameter WHERE key = 'som_crm_daily_won_leads_target')::integer,
-                0
+            ,cl.som_channel as channel
+            ,cl.source_id as source
+            ,cl.lost_reason_id as lost_reason
+            ,100 / (COALESCE(
+                (SELECT value FROM ir_config_parameter WHERE key = 'som_crm_daily_won_leads_target')::float,
+                1.0)
             )AS won_daily_target
+            ,(COALESCE(
+                (SELECT value FROM ir_config_parameter WHERE key = 'som_crm_daily_won_leads_target')::integer,
+                0)
+            )AS won_daily_target_static
+            ,case
+                when cl.stage_id = (select ID from crm_stage cs where is_won = true) then 'w'
+                when cl.active = false then 'l'
+                else 'f'
+            end as virtual_state
+            ,case when cl.stage_id = (select ID from crm_stage cs where is_won = true) then 1 else 0 end as count_won
+            ,case when (cl.active = true and cl.stage_id != (select ID from crm_stage cs where is_won = true)) then 1 else 0 end as count_flying
+            ,case when cl.active = false then 1 else 0 end as count_lost
+            ,(select count(*) from crm_lead cl) as count_total
+            ,case
+                when cl.active = true and cl.stage_id = 1 then 'Pendent 1r contacte'
+                when cl.active = true and cl.stage_id = 2 then 'Pendent 1r contacte amb factura'
+                when cl.active = true and cl.stage_id = 3 then 'Pendent rebre factura'
+                when cl.active = true and cl.stage_id = 7 then 'Pendent enviar simulació'
+                when cl.active = true and cl.stage_id = 8 then 'Informació enviada'
+                when cl.active = true and cl.stage_id = 4 then 'Simulació enviada'
+                when cl.active = true and cl.stage_id = 6 then 'Contractat'
+                when cl.active = true and cl.stage_id = 5 then 'Interès i diu que contractarà'
+                else 'Perdut'
+            end as active_stage
         """
         return select_str
 
