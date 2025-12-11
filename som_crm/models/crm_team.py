ade875a53439e338aa7a4421381e39a516a1a2f6
pedropote <pere.montagud@somenergia.coop>
2025-12-11 13:21:48 +0100

    ✨ Exclude employees with absences in lead assignment process

--- som_crm/models/crm_team.py
+++ som_crm/models/crm_team.py
@@ -6,12 +6,14 @@ from odoo.exceptions import UserError
 class CrmTeam(models.Model):
     _inherit = 'crm.team'
 
-    def get_random_member(self, exclude_team_leader=False):
+    def get_random_member(self, exclude_team_leader=False, exclude_absent_members=True):
         """ Returns a random member of the sales team. """
         self.ensure_one()
         user_member_ids = self.member_ids
         if exclude_team_leader:
             user_member_ids = self.member_ids.filtered(lambda x: x != self.user_id)
+        if exclude_absent_members:
+            user_member_ids = user_member_ids.filtered(lambda x: x.employee_id.is_present)
         if not user_member_ids:
             return False
         random_member_id = choice(user_member_ids)
