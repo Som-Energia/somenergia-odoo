a703464a3288e4d42583eccc94be375dab05eb4b
PauBoix <pau.boix@somenergia.coop>
2026-03-19 10:54:13 +0100

    🚧 wip blacklist
    Co-authored-by: Pere Montagud <pere.montagud@somenergia.coop>

--- som_crm/models/iap_tools_extend.py
+++ som_crm/models/iap_tools_extend.py
@@ -1,13 +1,28 @@
-try:
-    from odoo.addons.iap.tools.iap_tools import _MAIL_DOMAIN_BLACKLIST
-    if isinstance(_MAIL_DOMAIN_BLACKLIST, set):
-        _SOM_BLACKLISTED_DOMAINS = {
-            'telefonica.net',
-            'liquidrockentertainment.com',
-            'coac.cat',
-            'xtec.cat',
-        }
-        _MAIL_DOMAIN_BLACKLIST.update(_SOM_BLACKLISTED_DOMAINS)
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-except ImportError:
-    pass
+
+class SomCrmMailDomainBlacklist(models.Model):
+    _name = 'som.crm.mail.domain.blacklist'
+    _description = 'Mail Domain Blacklist'
+
+    name = fields.Char(string='Domain', required=True)
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
+
+    def _update_blacklist(self):
+        try:
+            from odoo.addons.iap.tools.iap_tools import _MAIL_DOMAIN_BLACKLIST
+            if isinstance(_MAIL_DOMAIN_BLACKLIST, set):
+                _MAIL_DOMAIN_BLACKLIST.update(self.search([]).mapped('name'))
+        except ImportError:
+            pass
+
+    def _register_hook(self):
+        super()._register_hook()
+        self._update_blacklist()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        records._update_blacklist()
+        return records
